4c9ce45b6319d68b6fca55da43bc8ff473d02ef7
Shaina G <shainagtherapy@gmail.com>
2025-08-19 12:42:13 -0700

    Update Comment Success

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -77,8 +77,6 @@ router.delete("/:hootId", verifyToken, async (req, res) => {
   }
 });
 
-
-
 router.post("/:hootId/comments", verifyToken, async (req, res) => {
   try {
     req.body.author = req.user._id;
@@ -93,6 +91,26 @@ router.post("/:hootId/comments", verifyToken, async (req, res) => {
 
     // Respond with the newComment:
     res.status(201).json(newComment);
+  } catch (err) {
+    res.status(500).json({ err: err.message });
+  }
+});
+
+router.put("/:hootId/comments/:commentId", verifyToken, async (req, res) => {
+  try {
+    const hoot = await Hoot.findById(req.params.hootId);
+    const comment = hoot.comments.id(req.params.commentId);
+
+    // ensures the current user is the author of the comment
+    if (comment.author.toString() !== req.user._id) {
+      return res
+        .status(403)
+        .json({ message: "You are not authorized to edit this comment" });
+    }
+
+    comment.text = req.body.text;
+    await hoot.save();
+    res.status(200).json({ message: "Comment updated successfully" });
   } catch (err) {
     res.status(500).json({ err: err.message });
   }
